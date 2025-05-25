577369eee6344fbe0273a0c4e4ecbd2f7fa3bc25
loops <128599880+loopsc@users.noreply.github.com>
2025-05-25 14:42:14 +1200

    Initial commit

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import "./styles.css";
-import render from "./modules/render";
-import { list } from "./modules/projects-list";
-
-document.addEventListener("DOMContentLoaded", () => {
-    list.init()
-    render();
-});
-
